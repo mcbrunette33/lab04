1acceccfbf7fa4249b69c9d7fde0f3a03126e2a3
michael <mcbrunette33@gmail.com>
2022-02-24 15:37:45 -0800

    added lab 07

---
+++ app.js
@@ -0,0 +1,14 @@
+function checkGuest() {
+    var guest = prompt("Your name?"); 
+    if(guest.toLowerCase() == "Mike"){
+        document.write("Welcome Back <Mike>!");
+    } else{
+        document.write(`Do you like Poker ${guest} ?`);
+    }
+    var answer = prompt("Do you like poker?");
+    while(answer.toLocaleLowerCase() != "yes"){
+        answer = prompt("Go away if you dont like poker! Reply yes to enter my page")
+    }
+    return guest; 
+} 
+//checkGuest();
